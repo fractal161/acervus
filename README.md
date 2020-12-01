2bf0ea97f14a13073a63bb07f8f5cef59ce936aa
Justin Yu <628snapdragon@gmail.com>
2020-12-01 00:49:24 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+A C++ implementation of Ryan Heise's Pagerank algorithm to (eventually) be used for NES Tetris.
---
+++ main.cpp
@@ -0,0 +1,10 @@
+// Sketch of what needs to happen
+/*
+Store all ranks in "surfaces." Use mmap to load each value into a gigantic array for quick lookup.
+
+Write a library for manipulating surfaces (adding pieces, evaluating decisions, etc.)
+
+Different program for actually running iterations of pagerank. Make sure it can check for convergence and all.
+
+Main method should use x11 and xdotool (?) to test the ranks on the James Clewett challenge (REMEMBER TO USE GUEST ACCOUNT)
+*/
