a6088939377cabe674a0c97773fea15341ca0c38
Justin Yu <628snapdragon@gmail.com>
2021-02-01 17:57:59 -0600

    Changed to a Gauss-Seidel method, stopped using mmap.

--- rank/vIter.cpp
+++ rank/vIter.cpp
@@ -1,6 +1,5 @@
 #include "surfaces.hpp"
 #include <sys/stat.h>
-#include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <math.h> // For stdev
@@ -14,9 +13,10 @@ const unsigned int NUM_SURFACES = std::pow(9, SURFACE_WIDTH - 1);
 const unsigned int RANK_SIZE = NUM_SURFACES * sizeof(float);
 const char* FILE_NAME = "../data/ranksEv9";
 const float initRank = 0.0f;
-const int nThreads = 4;
+const int nThreads = 1;
+const double lambda = 1.0;
 
-float* ranks[2];
+float* ranks;
 
 class RankStats{
 private:
@@ -30,15 +30,15 @@ private:
 
 public:
   RankStats();
-  void update(int row, int surface);
+  void update(float nRank, int surface);
   void print();
   void merge(RankStats& otherStats);
 
 };
 
-void setRanks(int start, int end, int row, RankStats* stats);
-float rank2(int iter, int stack);
-void rankOrient2(int iter, int* surface, int piece, int orientation, int pos, float* pieceRanks);
+void setRanks(int start, int end, RankStats* stats);
+float rank(int stack);
+void rankOrient(int* surface, int piece, int orientation, int pos, float* pieceRanks);
 
 int main(int argc, char* argv[]){
   int fd = open(FILE_NAME, O_RDWR|O_CREAT, 0666);
@@ -48,9 +48,7 @@ int main(int argc, char* argv[]){
   }
   struct stat stat_buf;
   fstat(fd, &stat_buf); //Not cross platform
-  bool mapped = false;
-  ranks[0] = new float[RANK_SIZE];
-  ranks[1] = new float[RANK_SIZE];
+  ranks = new float[RANK_SIZE];
 
   int iters;
   if(argc == 1){
@@ -63,7 +61,7 @@ int main(int argc, char* argv[]){
   if(stat_buf.st_size == 0){
     std::cout << "Initializing ranks\n";
     for(int i = 0; i < NUM_SURFACES; i++){
-      ranks[0][i] = initRank;
+      ranks[i] = initRank;
     }
   }
   else if(stat_buf.st_size != RANK_SIZE){
@@ -71,16 +69,17 @@ int main(int argc, char* argv[]){
     return 1;
   }
   else{
-    delete[] ranks[0];
-    ranks[0] = reinterpret_cast<float*>(mmap(nullptr, RANK_SIZE,
-      PROT_READ|PROT_WRITE,
-      MAP_FILE|MAP_SHARED,
-      fd, 0));
-    if(ranks[0] == MAP_FAILED){
-      std::cout << "Error\n";
-      return 1;
-    }
-    mapped = true;
+    // delete[] ranks;
+    // ranks = reinterpret_cast<float*>(mmap(nullptr, RANK_SIZE,
+    //   PROT_READ|PROT_WRITE,
+    //   MAP_FILE|MAP_SHARED,
+    //   fd, 0));
+    // if(ranks == MAP_FAILED){
+    //   std::cout << "Error\n";
+    //   return 1;
+    // }
+    // mapped = true;
+    read(fd, ranks, RANK_SIZE);
   }
 
   std::cout << "Beginning the ranking process...\n";
@@ -88,7 +87,6 @@ int main(int argc, char* argv[]){
   using pSeconds = std::chrono::duration<double, std::ratio<1, 1>>;
   auto trueStart = std::chrono::steady_clock::now();
   for(int i = 0; i < iters; i++){
-    int row = (i + 1) % 2;
     auto startTime = std::chrono::steady_clock::now();
     RankStats* stats = new RankStats[nThreads];
     std::thread rankThreads[nThreads];
@@ -96,7 +94,7 @@ int main(int argc, char* argv[]){
     for(int i = 0; i < nThreads; i++){
       int start = (i * NUM_SURFACES) / nThreads;
       int end = ((i + 1) * NUM_SURFACES) / nThreads;
-      rankThreads[i] = std::thread(setRanks, start, end, row, stats+i);
+      rankThreads[i] = std::thread(setRanks, start, end, stats+i);
     }
 
     for(int i = 0; i < nThreads; i++){
@@ -130,18 +128,18 @@ int main(int argc, char* argv[]){
     int nextStack = 0;
     int tCount = 0;
     for(int j = 0; j < NUM_SURFACES; j++){
-      if(ranks[iters % 2][j] > nextMax){
-        if(ranks[iters % 2][j] == lastMax){
+      if(ranks[j] > nextMax){
+        if(ranks[j] == lastMax){
           tCount++;
           if(tCount > tie){
-            nextMax = ranks[iters % 2][j];
+            nextMax = ranks[j];
             nextStack = j;
             tie++;
             break;
           }
         }
-        if(ranks[iters % 2][j] < lastMax){
-          nextMax = ranks[iters % 2][j];
+        if(ranks[j] < lastMax){
+          nextMax = ranks[j];
           nextStack = j;
         }
       }
@@ -153,12 +151,8 @@ int main(int argc, char* argv[]){
     lastMax = nextMax;
   }
 
-  if(mapped) munmap(ranks[0], RANK_SIZE);
-
   // Write final ranks to file if necessary
-  if(iters % 2 == 1 || !mapped){
-      write(fd, ranks[iters % 2], RANK_SIZE);
-  }
+  write(fd, ranks, RANK_SIZE);
   close(fd);
   return 0;
 }
@@ -185,18 +179,17 @@ void RankStats::print(){
   std::cout << printSurface(intToSurface(bestSurface)) << "\n" << bestRank << std::endl;
 }
 
-void RankStats::update(int row, int surface){
-  int prev = (row + 1) % 2;
+void RankStats::update(float nRank, int surface){
   nSurfaces++;
-  if(ranks[row][surface] > bestRank){
+  if(nRank > bestRank){
     bestSurface = surface;
-    bestRank = ranks[row][surface];
+    bestRank = nRank;
   }
-  double err = (double) abs(ranks[row][surface] - ranks[prev][surface]);
+  double err = (double) abs(ranks[surface] - nRank);
   maxErr = std::max(maxErr, err);
   totErr += err;
   totSquErr += err * err;
-  totRank += (double) ranks[row][surface];
+  totRank += (double) nRank;
 }
 
 void RankStats::merge(RankStats& otherStats){
@@ -211,82 +204,82 @@ void RankStats::merge(RankStats& otherStats){
   totRank += otherStats.totRank;
 }
 
-void setRanks(int start, int end, int row, RankStats* stats){
+void setRanks(int start, int end, RankStats* stats){
   for(int i = start; i < end; i++){
-    ranks[row][i] = rank2(row, i);
-    stats->update(row, i);
+    float nRank = rank(i);
+    stats->update(nRank, i);
+    ranks[i] = nRank;
   }
 }
 
-float rank2(int iter, int stack){
+float rank(int stack){
   int* surface = intToSurface(stack);
   float pieceRanks[] = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
 
   for(int i = 0; i < SURFACE_WIDTH; i++){
-    rankOrient2(iter, surface, I, 1, i, pieceRanks);
+    rankOrient(surface, I, 1, i, pieceRanks);
   }
   for(int i = 0; i < SURFACE_WIDTH - 1; i++){
     if(surface[i] == -2){
-      rankOrient2(iter, surface, L, 3, i, pieceRanks);
+      rankOrient(surface, L, 3, i, pieceRanks);
     }
     if(surface[i] == -1){
-      rankOrient2(iter, surface, T, 3, i, pieceRanks);
-      rankOrient2(iter, surface, S, 1, i, pieceRanks);
+      rankOrient(surface, T, 3, i, pieceRanks);
+      rankOrient(surface, S, 1, i, pieceRanks);
     }
     if(surface[i] == 0){
-      rankOrient2(iter, surface, O, 0, i, pieceRanks);
-      rankOrient2(iter, surface, L, 1, i, pieceRanks);
-      rankOrient2(iter, surface, J, 3, i, pieceRanks);
+      rankOrient(surface, O, 0, i, pieceRanks);
+      rankOrient(surface, L, 1, i, pieceRanks);
+      rankOrient(surface, J, 3, i, pieceRanks);
     }
     if(surface[i] == 1){
-      rankOrient2(iter, surface, T, 1, i, pieceRanks);
-      rankOrient2(iter, surface, Z, 1, i, pieceRanks);
+      rankOrient(surface, T, 1, i, pieceRanks);
+      rankOrient(surface, Z, 1, i, pieceRanks);
     }
     if(surface[i] == 2){
-      rankOrient2(iter, surface, J, 1, i, pieceRanks);
+      rankOrient(surface, J, 1, i, pieceRanks);
     }
   }
   for(int i = 0; i < SURFACE_WIDTH - 2; i++){
     if(surface[i] == 0 && surface[i + 1] == 0){
-      rankOrient2(iter, surface, T, 2, i, pieceRanks);
-      rankOrient2(iter, surface, J, 2, i, pieceRanks);
-      rankOrient2(iter, surface, L, 2, i, pieceRanks);
+      rankOrient(surface, T, 2, i, pieceRanks);
+      rankOrient(surface, J, 2, i, pieceRanks);
+      rankOrient(surface, L, 2, i, pieceRanks);
     }
     if(surface[i] == -1 && surface[i + 1] == 1){
-      rankOrient2(iter, surface, T, 0, i, pieceRanks);
+      rankOrient(surface, T, 0, i, pieceRanks);
     }
     if(surface[i] == 0 && surface[i + 1] == -1){
-      rankOrient2(iter, surface, J, 0, i, pieceRanks);
+      rankOrient(surface, J, 0, i, pieceRanks);
     }
     if(surface[i] == -1 && surface[i + 1] == 0){
-      rankOrient2(iter, surface, Z, 0, i, pieceRanks);
+      rankOrient(surface, Z, 0, i, pieceRanks);
     }
     if(surface[i] == 0 && surface[i + 1] == 1){
-      rankOrient2(iter, surface, S, 0, i, pieceRanks);
+      rankOrient(surface, S, 0, i, pieceRanks);
     }
     if(surface[i] == 1 && surface[i + 1] == 0){
-      rankOrient2(iter, surface, L, 0, i, pieceRanks);
+      rankOrient(surface, L, 0, i, pieceRanks);
     }
   }
 
   for(int i = 0; i < SURFACE_WIDTH - 3; i++){
     if(surface[i] == 0 && surface[i + 1] == 0 && surface[i + 2] == 0){
-      rankOrient2(iter, surface, I, 0, i, pieceRanks);
+      rankOrient(surface, I, 0, i, pieceRanks);
     }
   }
 
   delete[] surface;
 
   float pieceTotal = pieceRanks[0] + pieceRanks[1] + pieceRanks[2] + pieceRanks[3] + pieceRanks[4] + pieceRanks[5] + pieceRanks[6];
-  return pieceTotal / 7.0f + 1.0f;
+  return (float) lambda * pieceTotal / 7.0f + 1.0f;
 }
 
-void rankOrient2(int iter, int* surface, int piece, int orientation, int pos, float*pieceRanks){
-  iter = (iter + 1) % 2;
+void rankOrient(int* surface, int piece, int orientation, int pos, float*pieceRanks){
   int* newSurface = addPieceToSurface(surface, piece, orientation, pos, false);
   if(validSurface(newSurface)){
     int newNum = surfaceToInt(newSurface);
-    float newRank = ranks[iter][newNum];
+    float newRank = ranks[newNum];
     pieceRanks[piece] = std::max(pieceRanks[piece], newRank);
   }
   delete[] newSurface;
--- rank/vIterLarge.cpp
+++ rank/vIterLarge.cpp
@@ -1,6 +1,5 @@
 #include "surfaces.hpp"
 #include <sys/stat.h>
-#include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <math.h> // For stdev
@@ -19,7 +18,7 @@ const int nThreads = 5;
 
 // Surface * 7 + pieceType
 
-float* ranks[2];
+float* ranks;
 
 class RankStats{
 private:
@@ -33,16 +32,16 @@ private:
 
 public:
   RankStats();
-  void update(int row, int surface);
+  void update(float nRank, int surface);
   void print();
   void merge(RankStats& otherStats);
 
 };
 
-void setRanks(int start, int end, int row, RankStats* stats);
-float rank2(int iter, int stack);
-void rankOrient2(int iter, int* surface, int piece, int orientation, int pos, float* pieceRanks);
-void printTopNSurfaces(int n, int iters);
+void setRanks(int start, int end, RankStats* stats);
+float rank(int stack);
+void rankOrient(int* surface, int piece, int orientation, int pos, float* pieceRanks);
+void printTopNSurfaces(int n);
 
 int main(int argc, char* argv[]){
   int fd = open(FILE_NAME, O_RDWR|O_CREAT, 0666);
@@ -52,9 +51,7 @@ int main(int argc, char* argv[]){
   }
   struct stat stat_buf;
   fstat(fd, &stat_buf); //Not cross platform
-  bool mapped = false;
-  ranks[0] = new float[RANK_SIZE];
-  ranks[1] = new float[RANK_SIZE];
+  ranks = new float[RANK_SIZE];
 
   int iters;
   if(argc == 1){
@@ -67,7 +64,7 @@ int main(int argc, char* argv[]){
   if(stat_buf.st_size == 0){
     std::cout << "Initializing ranks\n";
     for(int i = 0; i < NUM_SURFACES; i++){
-      ranks[0][i] = initRank;
+      ranks[i] = initRank;
     }
   }
   else if(stat_buf.st_size != RANK_SIZE){
@@ -75,16 +72,17 @@ int main(int argc, char* argv[]){
     return 1;
   }
   else{
-    delete[] ranks[0];
-    ranks[0] = reinterpret_cast<float*>(mmap(nullptr, RANK_SIZE,
-      PROT_READ|PROT_WRITE,
-      MAP_FILE|MAP_SHARED,
-      fd, 0));
-    if(ranks[0] == MAP_FAILED){
-      std::cout << "Error\n";
-      return 1;
-    }
-    mapped = true;
+    // delete[] ranks[0];
+    // ranks[0] = reinterpret_cast<float*>(mmap(nullptr, RANK_SIZE,
+    //   PROT_READ|PROT_WRITE,
+    //   MAP_FILE|MAP_SHARED,
+    //   fd, 0));
+    // if(ranks[0] == MAP_FAILED){
+    //   std::cout << "Error\n";
+    //   return 1;
+    // }
+    // mapped = true;
+    read(fd, ranks, RANK_SIZE);
   }
 
   std::cout << "Beginning the ranking process...\n";
@@ -92,7 +90,6 @@ int main(int argc, char* argv[]){
   using pSeconds = std::chrono::duration<double, std::ratio<1, 1>>;
   auto trueStart = std::chrono::steady_clock::now();
   for(int i = 0; i < iters; i++){
-    int row = (i + 1) % 2;
     auto startTime = std::chrono::steady_clock::now();
     RankStats* stats = new RankStats[nThreads];
     std::thread rankThreads[nThreads];
@@ -100,7 +97,7 @@ int main(int argc, char* argv[]){
     for(int i = 0; i < nThreads; i++){
       int start = (i * NUM_SURFACES) / nThreads;
       int end = ((i + 1) * NUM_SURFACES) / nThreads;
-      rankThreads[i] = std::thread(setRanks, start, end, row, stats+i);
+      rankThreads[i] = std::thread(setRanks, start, end, stats+i);
     }
 
     for(int i = 0; i < nThreads; i++){
@@ -115,24 +112,22 @@ int main(int argc, char* argv[]){
     std::cout << "Iteration " << (i + 1) << " completed\n";
     std::cout << "Duration: " << std::chrono::duration_cast<pSeconds>(endTime - startTime).count() << "s\n";
     stats[0].print();
-    printTopNSurfaces(2, i + 1);
+    printTopNSurfaces(2);
     delete[] stats;
 
     startTime = endTime;
   }
+  // Compare ranks with mirror
 
   auto trueEnd = std::chrono::steady_clock::now();
   std::cout << "Total duration: " <<
     std::chrono::duration_cast<pSeconds>(trueEnd - trueStart).count() << "s\n\n";
 
   // List n best surfaces.
-  printTopNSurfaces(20, iters);
-  if(mapped) munmap(ranks[0], RANK_SIZE);
+  printTopNSurfaces(20);
 
   // Write final ranks to file if necessary
-  if(iters % 2 == 1 || !mapped){
-      write(fd, ranks[iters % 2], RANK_SIZE);
-  }
+  pwrite(fd, ranks, RANK_SIZE, 0);
   close(fd);
   return 0;
 }
@@ -159,18 +154,17 @@ void RankStats::print(){
   // std::cout << printSurfaceAndPiece(intToSurface(bestSurface / 7), bestSurface % 7) << "\n" << bestRank << std::endl;
 }
 
-void RankStats::update(int row, int surface){
-  int prev = (row + 1) % 2;
+void RankStats::update(float nRank, int surface){
   nSurfaces++;
-  if(ranks[row][surface] > bestRank){
+  if(nRank > bestRank){
     bestSurface = surface;
-    bestRank = ranks[row][surface];
+    bestRank = nRank;
   }
-  double err = (double) abs(ranks[row][surface] - ranks[prev][surface]);
+  double err = (double) abs(ranks[surface] - nRank);
   maxErr = std::max(maxErr, err);
   totErr += err;
   totSquErr += err * err;
-  totRank += (double) ranks[row][surface];
+  totRank += (double) nRank;
 }
 
 void RankStats::merge(RankStats& otherStats){
@@ -185,14 +179,15 @@ void RankStats::merge(RankStats& otherStats){
   totRank += otherStats.totRank;
 }
 
-void setRanks(int start, int end, int row, RankStats* stats){
+void setRanks(int start, int end, RankStats* stats){
   for(int i = start; i < end; i++){
-    ranks[row][i] = rank2(row, i);
-    stats->update(row, i);
+    float nRank = rank(i);
+    stats->update(nRank, i);
+    ranks[i] = nRank;
   }
 }
 
-float rank2(int iter, int stack){
+float rank(int stack){
   int* surface = intToSurface(stack / 7);
   int type = stack % 7;
   float pieceRanks[] = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
@@ -200,98 +195,98 @@ float rank2(int iter, int stack){
     case T:
     for(int i = 0; i < SURFACE_WIDTH - 1; i++){
       if(surface[i] == -1){
-        rankOrient2(iter, surface, T, 3, i, pieceRanks);
+        rankOrient(surface, T, 3, i, pieceRanks);
       }
       if(surface[i] == 1){
-        rankOrient2(iter, surface, T, 1, i, pieceRanks);
+        rankOrient(surface, T, 1, i, pieceRanks);
       }
     }
     for(int i = 0; i < SURFACE_WIDTH - 2; i++){
       if(surface[i] == 0 && surface[i + 1] == 0){
-        rankOrient2(iter, surface, T, 2, i, pieceRanks);
+        rankOrient(surface, T, 2, i, pieceRanks);
       }
       if(surface[i] == -1 && surface[i + 1] == 1){
-        rankOrient2(iter, surface, T, 0, i, pieceRanks);
+        rankOrient(surface, T, 0, i, pieceRanks);
       }
     }
     break;
     case J:
     for(int i = 0; i < SURFACE_WIDTH - 1; i++){
       if(surface[i] == 0){
-        rankOrient2(iter, surface, J, 3, i, pieceRanks);
+        rankOrient(surface, J, 3, i, pieceRanks);
       }
       if(surface[i] == 2){
-        rankOrient2(iter, surface, J, 1, i, pieceRanks);
+        rankOrient(surface, J, 1, i, pieceRanks);
       }
     }
     for(int i = 0; i < SURFACE_WIDTH - 2; i++){
       if(surface[i] == 0 && surface[i + 1] == 0){
-        rankOrient2(iter, surface, J, 2, i, pieceRanks);
+        rankOrient(surface, J, 2, i, pieceRanks);
       }
       if(surface[i] == 0 && surface[i + 1] == -1){
-        rankOrient2(iter, surface, J, 0, i, pieceRanks);
+        rankOrient(surface, J, 0, i, pieceRanks);
       }
     }
     break;
     case Z:
     for(int i = 0; i < SURFACE_WIDTH - 1; i++){
       if(surface[i] == 1){
-        rankOrient2(iter, surface, Z, 1, i, pieceRanks);
+        rankOrient(surface, Z, 1, i, pieceRanks);
       }
     }
     for(int i = 0; i < SURFACE_WIDTH - 2; i++){
       if(surface[i] == -1 && surface[i + 1] == 0){
-        rankOrient2(iter, surface, Z, 0, i, pieceRanks);
+        rankOrient(surface, Z, 0, i, pieceRanks);
       }
     }
     break;
     case O:
     for(int i = 0; i < SURFACE_WIDTH - 1; i++){
       if(surface[i] == 0){
-        rankOrient2(iter, surface, O, 0, i, pieceRanks);
+        rankOrient(surface, O, 0, i, pieceRanks);
       }
     }
     break;
     case S:
     for(int i = 0; i < SURFACE_WIDTH - 1; i++){
       if(surface[i] == -1){
-        rankOrient2(iter, surface, S, 1, i, pieceRanks);
+        rankOrient(surface, S, 1, i, pieceRanks);
       }
     }
     for(int i = 0; i < SURFACE_WIDTH - 2; i++){
       if(surface[i] == 0 && surface[i + 1] == 1){
-        rankOrient2(iter, surface, S, 0, i, pieceRanks);
+        rankOrient(surface, S, 0, i, pieceRanks);
       }
     }
     break;
     case L:
     for(int i = 0; i < SURFACE_WIDTH - 1; i++){
       if(surface[i] == -2){
-        rankOrient2(iter, surface, L, 3, i, pieceRanks);
+        rankOrient(surface, L, 3, i, pieceRanks);
       }
       if(surface[i] == 0){
-        rankOrient2(iter, surface, L, 1, i, pieceRanks);
+        rankOrient(surface, L, 1, i, pieceRanks);
       }
     }
     for(int i = 0; i < SURFACE_WIDTH - 2; i++){
       if(surface[i] == 0 && surface[i + 1] == 0){
-        rankOrient2(iter, surface, L, 2, i, pieceRanks);
+        rankOrient(surface, L, 2, i, pieceRanks);
       }
       if(surface[i] == 1 && surface[i + 1] == 0){
-        rankOrient2(iter, surface, L, 0, i, pieceRanks);
+        rankOrient(surface, L, 0, i, pieceRanks);
       }
     }
     break;
     case I:
     for(int i = 0; i < SURFACE_WIDTH - 3; i++){
-      rankOrient2(iter, surface, I, 1, i, pieceRanks);
+      rankOrient(surface, I, 1, i, pieceRanks);
       if(surface[i] == 0 && surface[i + 1] == 0 && surface[i + 2] == 0){
-        rankOrient2(iter, surface, I, 0, i, pieceRanks);
+        rankOrient(surface, I, 0, i, pieceRanks);
       }
     }
-    rankOrient2(iter, surface, I, 1, SURFACE_WIDTH - 3, pieceRanks);
-    rankOrient2(iter, surface, I, 1, SURFACE_WIDTH - 2, pieceRanks);
-    rankOrient2(iter, surface, I, 1, SURFACE_WIDTH - 1, pieceRanks);
+    rankOrient(surface, I, 1, SURFACE_WIDTH - 3, pieceRanks);
+    rankOrient(surface, I, 1, SURFACE_WIDTH - 2, pieceRanks);
+    rankOrient(surface, I, 1, SURFACE_WIDTH - 1, pieceRanks);
     break;
   }
 
@@ -301,21 +296,18 @@ float rank2(int iter, int stack){
   return pieceTotal * 9.0f / 56.0f - pieceRanks[type] * 7.0f / 56.0f + 1.0f;
 }
 
-void rankOrient2(int iter, int * surface, int piece, int orientation, int pos, float* pieceRanks){
-  iter = (iter + 1) % 2;
+void rankOrient(int* surface, int piece, int orientation, int pos, float* pieceRanks){
   int* newSurface = addPieceToSurface(surface, piece, orientation, pos, false);
   if(validSurface(newSurface)){
     int newNum = surfaceToInt(newSurface);
-    // float newRank = ranks[iter][newNum];
-    // pieceRanks[piece] = std::max(pieceRanks[piece], newRank);
     for(int i = 0; i < 7; i++){
-      pieceRanks[i] = std::max(pieceRanks[i], ranks[iter][newNum * 7 + i]);
+      pieceRanks[i] = std::max(pieceRanks[i], ranks[newNum * 7 + i]);
     }
   }
   delete[] newSurface;
 }
 
-void printTopNSurfaces(int n, int iters){
+void printTopNSurfaces(int n){
   // int n = 20;
   int tie = 1;
   float lastMax = 1.0e9;
@@ -325,18 +317,18 @@ void printTopNSurfaces(int n, int iters){
     int nextStack = 0;
     int tCount = 0;
     for(int j = 0; j < NUM_SURFACES; j++){
-      if(ranks[iters % 2][j] > nextMax){
-        if(ranks[iters % 2][j] == lastMax){
+      if(ranks[j] > nextMax){
+        if(ranks[j] == lastMax){
           tCount++;
           if(tCount > tie){
-            nextMax = ranks[iters % 2][j];
+            nextMax = ranks[j];
             nextStack = j;
             tie++;
             break;
           }
         }
-        if(ranks[iters % 2][j] < lastMax){
-          nextMax = ranks[iters % 2][j];
+        if(ranks[j] < lastMax){
+          nextMax = ranks[j];
           nextStack = j;
         }
       }
